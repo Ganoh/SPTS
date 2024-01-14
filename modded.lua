6ef8c3ff938fbf5a576cb26ecc450bd75242e294
Ganoh <156333524+Ganoh@users.noreply.github.com>
2024-01-14 12:51:40 +0700

    Update modded.lua

--- modded.lua
+++ modded.lua
@@ -124,7 +124,7 @@ local args = {
     }
 }
 while getgenv().respawn == true do
-wait()
+wait(1)
 game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
 end
    end,
