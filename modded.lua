d7ba6bddca9b4acf797038fb5c3d76e97c1b4990
Ganoh <156333524+Ganoh@users.noreply.github.com>
2024-07-22 02:40:24 +0700

    Update modded.lua

--- modded.lua
+++ modded.lua
@@ -1,6 +1,5 @@
 getgenv().SecureMode = true
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/Waza80/scripts/main/MacOSRayfield.lua'))()
-local Rayfieltoggle = loadstring(game:HttpGet('https://raw.githubusercontent.com/Waza80/scripts/main/RayfieldToggle.lua '))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/Waza80/scripts/main/RayfieldToggle.lua'))()
 local Window = Rayfield:CreateWindow({
    Name = "SPTS: Modded",
    LoadingTitle = "SPTS Modded script",
