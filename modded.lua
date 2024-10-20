946243bc57b0d6de6f50b0606e1371e0245a3e99
Ganoh <156333524+Ganoh@users.noreply.github.com>
2024-10-20 10:40:31 +0700

    Update modded.lua

--- modded.lua
+++ modded.lua
@@ -29,13 +29,13 @@ local Tab = Window:CreateTab("Autofarming Stuff", 4483362458) -- Title, Image
 local Section = Tab:CreateSection("AFK Farming stuff")
 getgenv().ms = true
 local Toggle = Tab:CreateToggle({
-   Name = "Auto MS:100Sp",
+   Name = "Auto MS:10No",
    CurrentValue = false,
    Callback = function(Value)
 getgenv().ms = Value
 local args = {
     [1] = {
-        [1] = "+MS21"
+        [1] = "+MS22"
     }
 }
 while getgenv().ms == true do
@@ -46,13 +46,13 @@ end
 })
 getgenv().jf = true
 local Toggle = Tab:CreateToggle({
-   Name = "Auto JF:100Sp",
+   Name = "Auto JF:10No",
    CurrentValue = false,
    Callback = function(Value)
    getgenv().jf = Value
 local args = {
     [1] = {
-        [1] = "+JF21"
+        [1] = "+JF22"
     }
 }
 while getgenv().jf == true do
