6f446f2bd1426819f8d2ff36648152ea050a2941
Ganoh <156333524+Ganoh@users.noreply.github.com>
2024-11-03 09:33:05 +0700

    Update modded.lua

--- modded.lua
+++ modded.lua
@@ -202,7 +202,7 @@ local Button = Tab:CreateButton({
 local vu = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:connect(function()
 vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-wait(1)
+wait(10)
 vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
    end,
 })
