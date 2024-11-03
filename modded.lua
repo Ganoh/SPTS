61680a81b8281a06e8e41e644539d2b4aa0915c7
Ganoh <156333524+Ganoh@users.noreply.github.com>
2024-11-03 10:18:31 +0700

    Update modded.lua

--- modded.lua
+++ modded.lua
@@ -214,6 +214,7 @@ game:GetService("Players").LocalPlayer.Idled:connect(function()
 vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 wait(10)
 vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+Rayfield:LoadConfiguration()
 end
    end,
 })
